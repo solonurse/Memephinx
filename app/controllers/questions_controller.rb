0d2d829ce2e681b5dc170fb7546a3b1068a20e52
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2023-11-28 23:54:48 +0900

    クイズデータ

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,21 +17,17 @@ class QuestionsController < ApplicationController
       @question = Question.find_by(id: question_id)
       session[:answered_question_count] += 1
       # ajaxでページ遷移する
-      respond_to do |format|
-
-      end
+      return render turbo_stream: turbo_stream.replace(@question)
     # 2問目~5問目
-    elsif session[:answered_question_count] > 0 && session[:answered_question_count] < 5
-      #正解していたら次の問題へ
+    elsif session[:answered_question_count] >= 1 && session[:answered_question_count] < 5
+    #   #正解していたら次の問題へ
       if user_answer == correnct_answer.answer
         question_id = session[:questions_order].shift! unless session[:questions_order].empty?
         @question = Question.find_by(id: question_id)
         session[:answered_question_count] += 1
         # ajaxでページ遷移する
-        respond_to do |format|
-        
-        end
-      # ゲームオーバーしたら問題数をリセットしてゲームオーバー画面へリダイレクト
+        return render turbo_stream: turbo_stream.replace(@question)
+      # # ゲームオーバーしたら問題数をリセットしてゲームオーバー画面へリダイレクト
       else
         reset_session
         redirect_to questions_gameover_path
