df11d0d15e74442e1e17df05a56a752977b45916
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2023-12-04 10:06:13 +0900

    ホスト名追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "memephinx.magia.runteq.jp"
+  # config.hosts << "memephinx.magia.runteq.jp"
 end
